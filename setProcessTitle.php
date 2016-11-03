95ab48f254ba808c233f3803016ba2d5960c9797
loganfreeman <shanhongcheng@hotmail.com>
2016-11-03 14:00:40 -0600

    Create setProcessTitle.php

---
+++ setProcessTitle.php
@@ -0,0 +1,16 @@
+    /**
+     * Set process name.
+     *
+     * @param string $title
+     * @return void
+     */
+    protected static function setProcessTitle($title)
+    {
+        // >=php 5.5
+        if (function_exists('cli_set_process_title')) {
+            @cli_set_process_title($title);
+        } // Need proctitle when php<=5.5 .
+        elseif (extension_loaded('proctitle') && function_exists('setproctitle')) {
+            @setproctitle($title);
+        }
+    }
