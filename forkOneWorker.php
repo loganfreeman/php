26ff1ddf60653f7a62bf451c6c6788fd5caafb75
loganfreeman <shanhongcheng@hotmail.com>
2016-11-03 13:53:51 -0600

    Create forkOneWorker.php

---
+++ forkOneWorker.php
@@ -0,0 +1,35 @@
+    /**
+     * Fork one worker process.
+     *
+     * @param Worker $worker
+     * @throws Exception
+     */
+    protected static function forkOneWorker($worker)
+    {
+        $pid = pcntl_fork();
+        // Get available worker id.
+        $id = self::getId($worker->workerId, 0);
+        // For master process.
+        if ($pid > 0) {
+            self::$_pidMap[$worker->workerId][$pid] = $pid;
+            self::$_idMap[$worker->workerId][$id]   = $pid;
+        } // For child processes.
+        elseif (0 === $pid) {
+            if ($worker->reusePort) {
+                $worker->listen();
+            }
+            if (self::$_status === self::STATUS_STARTING) {
+                self::resetStd();
+            }
+            self::$_pidMap  = array();
+            self::$_workers = array($worker->workerId => $worker);
+            Timer::delAll();
+            self::setProcessTitle('WorkerMan: worker process  ' . $worker->name . ' ' . $worker->getSocketName());
+            $worker->setUserAndGroup();
+            $worker->id = $id;
+            $worker->run();
+            exit(250);
+        } else {
+            throw new Exception("forkOneWorker fail");
+        }
+    }
